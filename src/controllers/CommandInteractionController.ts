b2e99cb5f9b93a927a434b9f81b6550900ee2840
Jackson <jrobe3221@gmail.com>
2021-06-02 15:38:07 -0500

    pushing

--- src/controllers/CommandInteractionController.ts
+++ src/controllers/CommandInteractionController.ts
@@ -4,7 +4,7 @@ import { CommandController } from "./CommandController";
 import { InteractionControllerBase } from "./InteractionControllerBase";
 
 export class CommandInteractionController extends InteractionControllerBase {
-  constructor(raw: RawInteractionObject, client: InteractiveClient) {
+    constructor(raw: RawInteractionObject, client: InteractiveClient) {
     super(raw, client);
   }
 
