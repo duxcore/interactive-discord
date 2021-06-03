f1d1da2bba7e6924ae6937a2905bc66ed7c8e7e4
Jackson <jrobe3221@gmail.com>
2021-06-02 21:12:56 -0500

    added command patch method

---
+++ src/util/updateCommand.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import InteractiveClient from "..";
+import { SlashCommand } from "../structures/SlashCommand";
+import { discord } from "./constraints";
+import { ApplicationCommand } from "./types/command";
+
+export default function updateCommand(cmd: SlashCommand, client: InteractiveClient): Promise<ApplicationCommand> {
+  return new Promise(async (resolve, reject) => {
+    const application = (await client.bot.fetchApplication());
+    const appurl = `${discord.api_url}/applications/${application.id}`
+    const isGlobal = (!cmd.guilds || cmd.guilds.length == 0);
+
+    if (isGlobal) {
+      const url = `${appurl}/commands`;
+      axios.patch(url, cmd.compile(), { headers: { 'Authorization': `Bot ${client.bot.token}` } }).then((dat) => {
+        const data = dat.data;
+        return resolve(data);
+      }).catch(err => reject(err.response.data));
+      return;
+    }
+
+    const guilds = (typeof cmd.guilds == "string" ? [cmd.guilds] : cmd.guilds);
+
+    guilds?.forEach(id => {
+      const url = `${appurl}/guilds/${id}/commands`;
+      axios.patch(url, cmd.compile(), { headers: { 'Authorization': `Bot ${client.bot.token}` } }).then((dat) => {
+        const data = dat.data;
+        return resolve(data);
+      }).catch(err => reject(err.response.data));
+      return;
+    })
+  });
+}
