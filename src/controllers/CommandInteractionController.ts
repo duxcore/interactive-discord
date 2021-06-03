43f5a3a2516865f8a92bab2d14b8944e4832b849
Jackson <jrobe3221@gmail.com>
2021-06-03 12:49:09 -0500

    Update Command Controller

--- src/controllers/CommandInteractionController.ts
+++ src/controllers/CommandInteractionController.ts
@@ -1,6 +1,5 @@
 import InteractiveClient from "..";
 import { RawInteractionObject } from "../util/types/interactions";
-import { CommandController } from "./CommandController";
 import { InteractionControllerBase } from "./InteractionControllerBase";
 
 export class CommandInteractionController extends InteractionControllerBase {
