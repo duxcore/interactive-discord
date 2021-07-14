57e69ecd125ca5e843f77dd545ac30ddd3549e82
Amitoj Singh <amitojsingh366@gmail.com>
2021-07-14 14:21:01 +0300

    Release 1.4.8

--- src/controllers/CommandInteractionController.ts
+++ src/controllers/CommandInteractionController.ts
@@ -3,14 +3,14 @@ import { RawInteractionObject } from "../util/types/interactions";
 import { InteractionControllerBase } from "./InteractionControllerBase";
 
 export class CommandInteractionController extends InteractionControllerBase {
-  private _name: String;
+  private _name: string;
 
   constructor(raw: RawInteractionObject, client: InteractiveClient) {
     super(raw, client);
     this._name = raw.data.name || "";
   }
 
-  get name(): String {
+  get name(): string {
     return this._name;
   }
 }
