7f26df92626acc8082bd2b482f734672169b0ae0
Amitoj Singh <amitojsingh366@gmail.com>
2021-07-14 14:15:17 +0300

    add name to command interaction

--- src/controllers/CommandInteractionController.ts
+++ src/controllers/CommandInteractionController.ts
@@ -3,7 +3,14 @@ import { RawInteractionObject } from "../util/types/interactions";
 import { InteractionControllerBase } from "./InteractionControllerBase";
 
 export class CommandInteractionController extends InteractionControllerBase {
-    constructor(raw: RawInteractionObject, client: InteractiveClient) {
+  private _name: String;
+
+  constructor(raw: RawInteractionObject, client: InteractiveClient) {
     super(raw, client);
+    this._name = raw.data.name || "";
+  }
+
+  get name(): String {
+    return this._name;
   }
 }
